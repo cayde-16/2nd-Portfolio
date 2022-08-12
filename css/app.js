13a6ec751c23f90b560d5bd1f5e651b9ced84535
Cade Heck <cadeheck16@gmail.com>
2022-08-12 19:30:04 -0400

    Updated Hero image and Logo Design

--- css/app.js
+++ css/app.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const header = document.querySelector('.header .container')
+const header = document.querySelector('.header.container')
 
 hamburger.addEventListener('click' ,()=> {
         hamburger.classList.toggle('active');
